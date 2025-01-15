f413e96b095c85ebcc27f6b8bfcb54bc01481ebd
SafaaC <safaaacherak@gmail.com>
2025-01-15 12:11:47 +0530

    Gateway service updated

--- gateway-service/src/main/java/com/example/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gateway_service/GatewayServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Enable Eureka client for service discovery
+
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
