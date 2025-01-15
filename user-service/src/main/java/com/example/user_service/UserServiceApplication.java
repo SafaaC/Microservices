954d79be1ac2d873a540578c2e3786de15789ea2
SafaaC <safaaacherak@gmail.com>
2025-01-15 12:13:22 +0530

    user service updated

--- user-service/src/main/java/com/example/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/example/user_service/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Enable Eureka Client for service registration
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
